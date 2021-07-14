b44609ae1dc0cafedead27c75404fab6c3f91385
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2021-07-15 00:01:21 +0300

    optimize code

--- source/helper.php
+++ source/helper.php
@@ -21,6 +21,8 @@ use \Joomla\CMS\Factory;
 /**
  * Helper for mod_yootheme_megamenu
  */
+
+//why abstract? who will be inherited from him?
 abstract class ModYoothemeMegamenuHelper
 {
 	/**
@@ -35,52 +37,75 @@ abstract class ModYoothemeMegamenuHelper
 	public static function getItems(&$params)
 	{
 		$db = Factory::getDbo();
+		$toplevelitems = $params->get('toplevelitems', []);
 
-		$toplevelitems = $params->get('toplevelitems');
-		$nritems = count($toplevelitems) + 1;;
+		// if empty, then do not connect to the database
+		if(count($toplevelitems) === 0)
+		{
+			return $toplevelitems;
+		}
+
+		$nritems = count($toplevelitems) + 1; // not used, can be removed
 		$count = 0;
+		$menu_ids = [];
+		$menu_titles = [];
+
+		// it's better to go through and collect data for one request to database
+		foreach ($toplevelitems as $item)
+		{
+			$menu_ids[] = (int) $item->internallink;
+		}
+
+		$query = $db->getQuery(true)
+			->select($db->quoteName(['id', 'title',]))
+			->from($db->quoteName('#__menu'))
+			->where($db->quoteName('id') . ' IN (' .  implode(',', $menu_ids) . ')');
+		$db->setQuery($query);
+		$result = $db->loadObjectList();
+
+		foreach ($result as $item)
+		{
+			$menu_titles[(int) $item->id] = $item->title;
+		}
 
 		# Check for each item if internal link title should be used and cleanup unnecessary fields
 		foreach ($toplevelitems as $item)
 		{
 			# Set the module name
 			$count++;
-			$item->modulename = (string) 'megamenu' . $count;
+			$item->modulename = 'megamenu' . $count;
+			$item->showdd = '';
+			$item->animation = '';
 
 			# If the link is internal and we need to use the linked menu item title, get it from the database
-			if ($item->linktype == 'internal' && $item->useinternallinktitle == '1')
+			if ($item->linktype === 'internal' && $item->useinternallinktitle === '1')
 			{
 				$item->externallink = '';
 				$item->target = '';
-				$query = $db->getQuery(true)
-					->select($db->quoteName('title'))
-					->from($db->quoteName('#__menu'))
-					->where($db->quoteName('id') . ' = ' . (int) $item->internallink);
-				$db->setQuery($query);
-				$item->title = $db->loadResult();
+				$item->title = $menu_titles[(int) $item->id];
 			}
 
 			# If the link is external clear the internal link and the internal link title
-			if ($item->linktype == 'external')
+			if ($item->linktype === 'external')
 			{
 				$item->internallink = '';
 				$item->useinternallinktitle = '';
 			}
 
 			# If the item is a link construct it
-			if ($item->islink == '1')
+			if ($item->islink === '1')
 			{
-				if ($item->linktype == 'internal')
+				if ($item->linktype === 'internal')
 				{
 					$item->link = 'index.php?Itemid=' . $item->internallink;
 					$item->target = '';
 				}
 
-				if ($item->linktype == 'external')
+				if ($item->linktype === 'external')
 				{
 					$item->link = $item->externallink;
 					# Check if the external link needs to be opened in a new browser tab
-					if ($item->target == '_blank')
+					if ($item->target === '_blank')
 					{
 						$item->target = ' target="_blank"';
 					}
@@ -93,13 +118,9 @@ abstract class ModYoothemeMegamenuHelper
 			}
 
 			# Check if a dropdown indicator needs to be shown
-			if ($item->showdd == '1')
+			if ($item->showdd === '1')
 			{
 				$item->showdd = ' <span uk-icon="icon: ' . $item->ddicon . '"></span>';
-			}
-			else
-			{
-				$item->showdd = '';
 			}
 
 			# Check if an offset needs to be set
@@ -108,27 +129,24 @@ abstract class ModYoothemeMegamenuHelper
 				$item->offset = 'offset:' . $item->offset . ';';
 			}
 
-			# Check for an item postion
+			# Check for an item position
 			if ($item->position)
 			{
 				$item->position = 'pos:' . $item->position . ';';
 			}
 
 			# Check if item is a single column dropdown, if so set columns to 1.
-			if ($item->ddkind == 'single')
+			if ($item->ddkind === 'single')
 			{
 				$item->columns = 1;
 			}
 
 			# Check for animation
-			if ($item->useanimation == '1')
+			if ($item->useanimation === '1')
 			{
 				$item->animation = 'animation:' . $item->animation  . ';duration:' . $item->animationduration . ';';
 			}
-			else
-			{
-				$item->animation = '';
-			}
+
 		}
 
 		return $toplevelitems;
